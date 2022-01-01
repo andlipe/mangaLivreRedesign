a88f1af2137583e93bdf441695137f63db631642
André Moura <andrefelipebarbosamoura@gmail.com>
2022-01-01 10:49:55 -0300

    fix: adjust tests from pages

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@ module.exports = {
   images: {
     domains: ['live.staticflickr.com'],
   },
-  webpack: (config, { webpack }) => {
-    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
-    return config
-  },
   redirects() {
     return [
       {
