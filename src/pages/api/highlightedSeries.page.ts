246ed129e66570ca8b71721a8d0a21c72a006f02
André Moura <andrefelipebarbosamoura@gmail.com>
2022-01-01 15:00:40 -0300

    fix: adjust images not loading

--- src/pages/api/highlightedSeries.page.ts
+++ src/pages/api/highlightedSeries.page.ts
@@ -15,8 +15,7 @@ export default async (_req: NextApiRequest, res: NextApiResponse<Data[]>) => {
     {
       id: 2697,
       chapter: '79.2',
-      image:
-        'https://static3.mangalivre.net/destaques/-xzgF7dWuEJOauTaw4xEJw/2697/how-not-summon-demon.png',
+      image: '/hnsdl.png',
       background: '7D1438',
       time: '2022-01-01T14:29:07-03:00',
       selected: true,
@@ -24,8 +23,7 @@ export default async (_req: NextApiRequest, res: NextApiResponse<Data[]>) => {
     {
       id: 2412,
       chapter: '360',
-      image:
-        'https://static3.mangalivre.net/destaques/3wPz0osUAbdyQiyMCqQZtw/2412/tales-of-demons-gods.png',
+      image: '/Tales.png',
       background: 'D37C12',
       time: '2022-01-01T14:04:25-03:00',
       selected: false,
@@ -33,8 +31,7 @@ export default async (_req: NextApiRequest, res: NextApiResponse<Data[]>) => {
     {
       id: 6938,
       chapter: '96',
-      image:
-        'https://static3.mangalivre.net/destaques/P9eeqB7OlsoilfoguY3S_Q/6938/ijiranaide-nagaroto.png',
+      image: '/Ijiranaide.png',
       background: '1099CD',
       time: '2022-01-01T13:50:02-03:00',
       selected: false,
@@ -42,8 +39,7 @@ export default async (_req: NextApiRequest, res: NextApiResponse<Data[]>) => {
     {
       id: 7702,
       chapter: '179',
-      image:
-        'https://static3.mangalivre.net/destaques/-_zttjheTKXCNaOBLeYOHQ/7702/sololevel.png',
+      image: '/Solem.png',
       background: '2F2828',
       time: '2022-01-01T13:41:39-03:00',
       selected: false,
@@ -51,8 +47,7 @@ export default async (_req: NextApiRequest, res: NextApiResponse<Data[]>) => {
     {
       id: 7247,
       chapter: '856',
-      image:
-        'https://static3.mangalivre.net/destaques/CgYxmgNjMEiN3-7OldU9Jw/7247/apotheosis.png',
+      image: '/Apotheosis.png',
       background: '1E142F',
       time: '2022-01-01T13:41:18-03:00',
       selected: false,
@@ -60,8 +55,7 @@ export default async (_req: NextApiRequest, res: NextApiResponse<Data[]>) => {
     {
       id: 1764,
       chapter: '218',
-      image:
-        'https://static3.mangalivre.net/destaques/o3jp6s6esy31KXPoUPk3DQ/1764/akatsuki-4.png',
+      image: '/Akatsuki.png',
       background: '1B0924',
       time: '2021-12-31T12:06:22-03:00',
       selected: false,
@@ -69,8 +63,7 @@ export default async (_req: NextApiRequest, res: NextApiResponse<Data[]>) => {
     {
       id: 10255,
       chapter: '69',
-      image:
-        'https://static3.mangalivre.net/destaques/mNfMDYqo-nMazTdb9QuBow/10255/tougen-anki.png',
+      image: '/Tougen.png',
       background: '200B06',
       time: '2021-12-31T10:57:06-03:00',
       selected: false,
